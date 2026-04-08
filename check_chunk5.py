bb36db0fe027ddc3b45a6114c3e3c9c03d41b8f7
awaisirfan54 <wasigoldca@gmail.com>
2026-04-08 12:55:02 +0500

    chore: remove debug scripts

--- check_chunk5.py
+++
@@ -1,9 +0,0 @@
-﻿import chromadb
-client = chromadb.PersistentClient(path='./data/chroma')
-col = client.get_collection('buildcore')
-results = col.get(where={'document_id': 'MAINT-FLT-03-forklift-toyota-8fgf25'}, include=['documents', 'metadatas'])
-for i, (doc, meta) in enumerate(zip(results['documents'], results['metadatas'])):
-    title = meta.get('section_title', '')
-    if 'PRE-START' in title.upper():
-        print('Length:', len(doc))
-        print(doc)
